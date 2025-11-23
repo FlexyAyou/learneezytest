a1d3fa8e352d8705b050e1279dadd6a3b18c2e68
abibou-1506 <abibou.ndiaye.dev@gmail.com>
2025-11-23 19:23:54 +0000

    FIX
    Add useEffect

--- src/pages/admin/EditCoursePage.tsx
+++ src/pages/admin/EditCoursePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
